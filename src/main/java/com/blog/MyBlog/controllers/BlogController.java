5cfbbc0b8ba0e6fe2e2c798de0197d70fe6a9525
ArtemPuzikov <Artdmpuzikov@mail.ru>
2020-05-06 23:30:15 +0300

    add page /blog/add and method in BlogController

--- src/main/java/com/blog/MyBlog/controllers/BlogController.java
+++ src/main/java/com/blog/MyBlog/controllers/BlogController.java
@@ -2,10 +2,13 @@ package com.blog.MyBlog.controllers;
 
 import com.blog.MyBlog.models.Post;
 import com.blog.MyBlog.repositories.PostRepository;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework. beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
@@ -20,8 +23,15 @@ public class BlogController {
         return "blog-main";
     }
 
-    @GetMapping("/blog/add ")
+    @GetMapping("/blog/add")
     public String blogAdd(Model model) {
-        return "blog-add ";
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title,@RequestParam String anons,@RequestParam String fullText, Model model) {
+        Post post = new Post(title,anons,fullText);
+        postRepository.save(post);
+        return "redirect:/blog";
     }
 }
